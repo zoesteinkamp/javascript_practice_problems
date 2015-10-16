0498b9e5e5191da84798fa982bb3011b337ed29c
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2015-10-15 21:32:03 -0600

    October Problem 2
    Palindrome solving problem

--- october_problems.js
+++ october_problems.js
@@ -14,4 +14,20 @@ function PrimeTime(value) {
     return value > 1;
 }
 
-// But the obvious flaw is that this might take a bit of time to run through
+//Using the JavaScript language, have the function PalindromeTwo(str) take the str parameter
+//being passed and return the string true if the parameter is a palindrome, (the string is the same
+//forward as it is backward) otherwise return the string false. The parameter entered may have punctuation
+//and symbols but they should not affect whether the string is in fact a palindrome. For example:
+//"Anne, I vote more cars race Rome-to-Vienna" should return true.
+
+
+function PalindromeTwo(str) {
+var backwards=str.split('').reverse().join('').replace(/\s+/g, '');
+var other = str.replace(/\s+/g, '');
+if(backwards===other){
+    return true
+}else{
+    return false
+  }
+}
+PalindromeTwo("a war at tarawa") // returns true
